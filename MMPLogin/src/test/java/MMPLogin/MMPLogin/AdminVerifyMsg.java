0bfe3939f5e6558ff77fa000094a6ff8af8fa4a6
ktashamai <112670269+ktashamai@users.noreply.github.com>
2023-03-30 23:10:15 -0700

    removed some lines

--- MMPLogin/src/test/java/MMPLogin/MMPLogin/AdminVerifyMsg.java
+++ MMPLogin/src/test/java/MMPLogin/MMPLogin/AdminVerifyMsg.java
@@ -11,19 +11,6 @@ import org.testng.annotations.Test;
 public class AdminVerifyMsg {
 	WebDriver driver;
 
-	/*@BeforeClass
-public void instantiateDriver() throws InterruptedException {
-		
-		System.setProperty("WebDriver.chrome.driver",
-				"/Users/AshaPrathib/eclipse-workspace/com.qa.actitime/atbrowser/chromedriver109");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-allow-origins=*"); 
-		driver=new ChromeDriver(options);
-		launchBrowser("http://96.84.175.78/MMP-Release2-Integrated-Build.6.8.000/portal/login.php");
-		//launchBrowser("http://96.84.175.78/MMP-Release2-Admin-Build.2.1.000/login.php");
-		
-	}*/
-
 @BeforeClass
 public void instantiateDriver1() throws InterruptedException {
 	
@@ -49,17 +36,12 @@ public void launchBrowser(String url) throws InterruptedException
 	
 	public void VerifyMessage() throws InterruptedException {
 		
-	/*	AdminLoginPage adminloginPage = new AdminLoginPage(driver);
-		AdminHomePage adminhomepage=adminloginPage.login("ria1","Ria12345");
-		PatientMsgPage pmp=new PatientMsgPage(driver);
-		pmp.navigate_module("Messages");
-		pmp.PatientMsg();
-	*/
 		
 		AdminLoginPage adminloginpage=new AdminLoginPage(driver);
 		AdminHomePage adminhomepage=adminloginpage.login("Ben@123","Frank@123");
 		adminloginpage.navigate_module("Messages");
 		AdminMsgPage adminmsgpage=new AdminMsgPage(driver);
+		driver.close();
 		adminmsgpage.AdminMsg();
 		
 }
